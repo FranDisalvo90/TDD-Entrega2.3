3bff964d939aba8f34d5d77d6947ba787ba63f83
patitonav <pato@pato-VirtualBox.(none)>
2013-11-14 15:42:12 -0300

    Test for Time control

---
+++ src/test/java/ar/fi/uba/td/testFramework/nestedSuitesTest/TestCase15.java
@@ -0,0 +1,34 @@
+package ar.fi.uba.td.testFramework.nestedSuitesTest;
+
+import ar.fi.uba.td.testFramework.Assert;
+import ar.fi.uba.td.testFramework.TestCase;
+import ar.fi.uba.td.testFramework.TestContext;
+import ar.fi.uba.td.testFramework.TestFailedException;
+
+public class TestCase15 extends TestCase {
+	int time;
+
+	public TestCase15(String nameTest) {
+		super(nameTest);
+		if (nameTest == "SLEEP1"){
+			this.time = 1000;
+		} else if (nameTest == "SLEEP5"){
+			this.time = 5000;
+		} else {
+			this.time = 0;
+		}
+	}
+
+	@Override
+	public void runTest(TestContext context) throws TestFailedException {
+		try {
+			Thread.sleep(this.time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		MockClass mock = (MockClass) context.getElement("mockA");
+		Assert.assertEquals(mock.getText(), "Using TestSuiteA context!",
+				"Passing test");
+	}
+
+}
